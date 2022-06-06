b759e306de218fa57ed779ac7ea4a6894acc4669
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2022-06-06 09:58:43 +0300

    Set up Expo project with TypeScript

---
+++ src/components/AppScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+import Colors from '../helpers/Colors'
+
+const AppScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: Colors.secondary,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default AppScreen
---
+++ src/helpers/Colors.tsx
@@ -0,0 +1,14 @@
+export default {
+    primary: '#6b2c2c',
+    primary_faded: '#945c5c',
+    secondary: '#ede1e0',
+
+    accent: '#f76e2f',
+    package: '#f9e9de',
+    shadow: '#755351',
+
+    black: '#000',
+    grey: '#414141',
+    red: '#b22439',
+    white: '#fff'
+}
