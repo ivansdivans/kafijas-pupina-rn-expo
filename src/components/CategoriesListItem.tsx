a0af64dbeaf88cf213d7953fb17bb89c46077b2a
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2022-07-17 13:10:02 +0300

    Create CategoriesListItem and render categories

---
+++ src/components/CategoriesListItem.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+	Text,
+	View,
+	TouchableOpacity,
+	ViewStyle,
+	StyleSheet,
+	TextStyle,
+} from "react-native";
+
+import AppIcon from "./AppIcon";
+import appStyles from "../styles/Application";
+
+interface Props {
+	onPress: () => void;
+	iconName: string;
+	categoryName: string;
+}
+
+const CategoriesListItem: React.VFC<Props> = ({
+	onPress,
+	iconName,
+	categoryName,
+}) => (
+	<TouchableOpacity style={styles.container} onPress={onPress}>
+		<View style={styles.iconBlock}>
+			<AppIcon name={iconName} size={80} />
+		</View>
+		<View style={styles.textBlock}>
+			<Text style={styles.title}>{categoryName}</Text>
+		</View>
+	</TouchableOpacity>
+);
+
+interface Style {
+	container: ViewStyle;
+	iconBlock: ViewStyle;
+	textBlock: ViewStyle;
+	title: TextStyle;
+}
+
+const stylesObj: Style = {
+	container: {
+		...appStyles.fullWidth,
+		...appStyles.inlineContainer,
+		minHeight: 50,
+		marginVertical: 10,
+		padding: 10,
+	},
+	iconBlock: {
+		width: "30%",
+	},
+	textBlock: {
+		width: "70%",
+		justifyContent: "center",
+		marginLeft: 20,
+	},
+	title: {
+		fontSize: 48,
+	},
+};
+
+const styles = StyleSheet.create<Style>(stylesObj);
+
+export default CategoriesListItem;
